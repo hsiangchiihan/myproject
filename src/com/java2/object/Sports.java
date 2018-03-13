57fb03db860d9b66e97c985b87222fefdfbefb0d
hsiangchiihan <hsianghan11235@gmail.com>
2018-03-13 14:08:47 +0800

    text3

---
+++ src/com/java2/object/Sports.java
@@ -0,0 +1,12 @@
+package com.java2.object;
+
+public class Sports {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public String name;
+		public int age;
+		private String favsport;
+	}
+
+}
