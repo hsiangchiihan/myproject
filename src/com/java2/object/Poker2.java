3e12d6ee066f060b8992844f1e17d6edcd8b7932
hsiangchiihan <hsianghan11235@gmail.com>
2018-04-09 15:10:30 +0800

    new text5

--- src/com/java2/object/Poker2.java
+++ src/com/java2/object/Poker2.java
@@ -9,13 +9,18 @@ public class Poker2 {
 		// TODO Auto-generated method stub
     Random random = new Random();
     ArrayList<Integer> cards = new ArrayList<>();
-    ArrayList<Integer> dealcards =new ArrayList<>();
-    String flowers = "SHDC";
+    ArrayList<Integer> deal_cards =new ArrayList<>();    
     int randomnum =0;
+    for (int j=0; j < 52 ; j++ ){    
+        cards.add(j+1);
+      }
+    for (int j=0; j < 52 ; j++ ){
+        randomnum = (int)(Math.random() * cards.size());   
+        deal_cards.add(cards.get(randomnum));   
+        cards.remove(randomnum);    
+      }
     
-    
-    	
     }
-	}
+}
 
 
