51aac1ea749eafaa3d46172a92a66fc40b11c526
hsiangchiihan <hsianghan11235@gmail.com>
2018-06-06 00:04:38 +0800

    new text21

--- src/com/java2/oo/VendingMachine.java
+++ src/com/java2/oo/VendingMachine.java
@@ -4,18 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
+
 public class VendingMachine {
+	
 	int total =0;
 	List<Drink> drinks =new ArrayList<>();
 	
 	public void on(){
-		System.out.println("Please insert coins:");
+		System.out.println("Please enter drink's number:");
 		for (int i=0;i<drinks.size();i++){
 			Drink drink =drinks.get(i);
 			System.out.println(drink.getId()+"\t"+drink.getName()+"\t"+drink.getPrice());			
 		}
 		Scanner s =new Scanner(System.in);
-		
+	    int data =-1;
+	    while(data !=0){
+	    	data =s.nextInt();
+	    	switch(data){
+	    	case 1:
+	    		System.out.println("You chose Coke.");
+	    		break;
+	    	case 2:
+	    		System.out.println("You chose Juice.");
+	    		break;
+	    	case 3:
+	    		System.out.println("You chose Sprite.");
+			case 0:
+				break;
+			}
+	    }
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/com/java2/oo/VendingMain.java
+++ src/com/java2/oo/VendingMain.java
@@ -4,11 +4,53 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class VendingMain {
+/*public VendingMain(){	
+}*/
 
+/*int price =0;
+public int getShopping(int i,String p)
+{
+	if(i==1)
+	{
+		if(p<tokens[2])
+		{
+			System.out.println("Not enough money!");
+		}
+		else
+		{
+			price=p-tokens[2];
+		}
+	}
+	if(i==2)
+	{
+		if(p<tokens[4])
+		{
+			System.out.println("Not enough money!");
+		}
+		else
+		{
+			price=p-tokens[4];
+		}
+	}
+	if(i==3)
+	{
+		if(p<tokens[6])
+		{
+			System.out.println("Not enough money!");
+		}
+		else
+		{
+			price=p-token[6];
+		}
+	}	
+	return price;
+}*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
     VendingMachine vm =new VendingMachine();
     FileReader fr;
     try {
@@ -23,6 +65,10 @@ public class VendingMain {
 			vm.drinks.add(new Drink(i+1,name,price));
 		}
 		vm.on();
+		System.out.println(" ");
+		System.out.print("To pay the bill:");
+		Scanner sp=new Scanner(System.in);
+		int n1 =sp.nextInt();
 	} catch (FileNotFoundException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
@@ -31,5 +77,5 @@ public class VendingMain {
 		e.printStackTrace();
 	}
 	}
-
+	
 }
