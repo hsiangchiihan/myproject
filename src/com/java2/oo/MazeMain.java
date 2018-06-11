1e4de0b3b2537de6e3bc0ed60486565630a3b832
hsiangchiihan <hsianghan11235@gmail.com>
2018-06-12 00:59:00 +0800

    new text23

--- src/com/java2/oo/MazeMain.java
+++ src/com/java2/oo/MazeMain.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MazeMain {
  public MazeMain(){
@@ -15,7 +17,12 @@ public class MazeMain {
 		String tokens[] =line.split(",");
 		int col =Integer.parseInt(tokens[0]);
 		int row	=Integer.parseInt(tokens[1]);
-		
+		for(int i=0;i<col;i++){
+			
+		}
+		for(int j=0;j<row;j++){
+			
+		}
 	} catch (FileNotFoundException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
@@ -24,12 +31,18 @@ public class MazeMain {
 		e.printStackTrace();
 	}
  }
+ 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new MazeMain();
-    
+		ArrayList<Integer> numList = new ArrayList<Integer>(); 
+		for(int i = 1; i<= 5; i++){ 
+		numList.add(i+5); 
+		} 
+		new Randomnum(numList).selectRandom(); 
+		} 
 	}
-	
+ 
 	class Maze{
 		int row ,col;
 		int trapCount;
@@ -47,4 +60,22 @@ public class MazeMain {
     	int hp =100;
     	int pos =0;
     }
-}
+    class Randomnum{
+    	ArrayList<Integer> checkList;
+    	Randomnum(ArrayList<Integer> num){
+    		this.checkList =num;
+    	}
+    	void selectRandom(){
+    		int i=1;
+    		int times =1;
+    		int count =5;
+    	    while(times<=5){
+    	    	i = checkList.get(new Random().nextInt(count--));
+    	    	System.out.println(i);
+    	    	checkList.remove((Object)i);
+    	    	times++;
+    	    }
+    	}
+    }
+    
+
