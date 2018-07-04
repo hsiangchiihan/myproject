9f45ae05b15e1823c97327628930eea9e8ed2369
hsiangchiihan <hsianghan11235@gmail.com>
2018-07-04 09:06:56 +0800

    Stepik practice3

--- src/com/stepik/practice/Introduction.java
+++ src/com/stepik/practice/Introduction.java
@@ -1,5 +1,6 @@
 package com.stepik.practice;
 
+import java.text.DecimalFormat;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -45,12 +46,40 @@ public class Introduction {
 		int answer =scanner.nextInt();
 		System.out.println(answer/10);*/
 		
-		Scanner scanner =new Scanner(System.in);
+		/*Scanner scanner =new Scanner(System.in);
 		int ans =scanner.nextInt();
 		int hun =ans/100;
 		int ten =(ans/10)%10;
 		int one =((ans%100)*100)%10;
-		System.out.println(hun+ten+one);
+		System.out.println(hun+ten+one);*/
+		
+		/*System.out.println("Please insert the distances from two cities: ");
+		Scanner sc1 =new Scanner(System.in);
+		float distance =sc1.nextFloat();
+		System.out.println("Please insert the travel time by bus in hours:");
+		Scanner sc2 =new Scanner(System.in);
+		float time =sc2.nextFloat();
+		System.out.println(distance/time);*/
+		
+		/*Scanner sc =new Scanner(System.in);
+		float ans =sc.nextFloat();
+		System.out.println((ans*10)%100);*/
+		
+		/*DecimalFormat formatter = new DecimalFormat("#.#");
+		System.out.println(formatter.format(0.9));*/
+
+	    Scanner scanner = new Scanner(System.in);
+	    double d = Math.abs(scanner.nextDouble());
+	    d = Math.floor(d*10);
+	    String s = Integer.toString((int)d);
+	    System.out.println(s.charAt(s.length()-1));
 	}
+	/*public static int getDecimalBits2(double target) {
+	    if (0.0 == target % 1) {
+	        return 0;
+	    } else {
+	        return Double.toString(target).split("\\.")[1].length();    
+	    }
+	}*/
 
 }
